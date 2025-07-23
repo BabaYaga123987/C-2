94504a6a7f8c55fa343107499de943116f098483
BabaYaga123987 <nezenecdmitrij45@gmail.com>
2025-07-23 18:25:22 +0200

    homework2

--- C#2/Program.cs
+++ C#2/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("А почему в ресторане ей никогда не нравится то,\nчто заказала она, и всегда нравится то, что заказал я?\nИ она начинает есть у меня из тарелки. Я ей говорю: \n\"Закажи себе то же самое\". Она говорит:\"Зачем?\nЯ только попробовать\". И съедает половину.\n\tк. ф. \"О чем говорят мужчины\"");
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your age");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your height");
+            double height = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your gender (m/f)");
+            string gender = Console.ReadLine();
+            Console.WriteLine("Enter your favorite letter");
+            char letter = char.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your city");
+            string city = Console.ReadLine();
+            Console.WriteLine($"Enter your name: {name}");
+            Console.WriteLine($"Enter your age: {age}");
+            Console.WriteLine($"Enter your height: {height}");
+            Console.WriteLine($"Enter your gender (m/f): {gender}");
+            Console.WriteLine($"Enter your favorite letter: {letter}");
+            Console.WriteLine($"Enter your city: {city}");
         }
     }
 }
