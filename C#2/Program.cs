3ae5340b3bdeaa355448f693d9025a8d7635b7f7
BabaYaga123987 <nezenecdmitrij45@gmail.com>
2025-08-23 12:06:43 +0200

    Refactor leap year check and number counting logic
    Simplified the leap year check by consolidating conditions into a single if statement for clarity. Refactored the counting of positive and negative numbers to use counters, enhancing readability and maintainability. Updated output statements to utilize string interpolation for improved formatting.

--- C#2/Program.cs
+++ C#2/Program.cs
@@ -6,11 +6,7 @@
         {
             Console.WriteLine("Введите год:");
             int year = int.Parse(Console.ReadLine());
-            if (year % 400 == 0)
-            {
-                Console.WriteLine("Год высокосный");
-            }
-            else if (year % 4 == 0 && year % 100 != 0)
+            if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0)
             {
                 Console.WriteLine("Год высокосный");
             }
@@ -20,6 +16,7 @@
             }
 
 
+
             Console.WriteLine("Введите первое целое число");
             int firstNumber = int.Parse(Console.ReadLine());
             Console.WriteLine("Введите второе целое число");
@@ -27,31 +24,18 @@
             Console.WriteLine("Введите третье целое число");
             int thirdNumber = int.Parse(Console.ReadLine());
 
-            if (firstNumber == 0 || secondNumber == 0 || thirdNumber == 0)
-            {
-                Console.WriteLine("Пожалуйста, вводите только позитивные или отрицательные числа (кроме 0)");
-            }
-            else if (firstNumber > 0 && secondNumber > 0 && thirdNumber > 0)
-            {
-                Console.WriteLine("Количество положительных чисел: 3");
-                Console.WriteLine("Количество отрицательных чисел: 0");
-            }
-            else if ((firstNumber > 0 && secondNumber > 0) || (secondNumber > 0 && thirdNumber > 0) 
-                || (firstNumber > 0 && thirdNumber > 0))
-            {
-                Console.WriteLine("Количество положительных чисел: 2");
-                Console.WriteLine("Количество отрицательных чисел: 1");
-            }
-            else if (firstNumber > 0 || secondNumber > 0 || thirdNumber > 0)
-            {
-                Console.WriteLine("Количество положительных чисел: 1");
-                Console.WriteLine("Количество отрицательных чисел: 2");
-            }
-            else
-            {
-                Console.WriteLine("Количество положительных чисел: 0");
-                Console.WriteLine("Количество отрицательных чисел: 3");
-            }
+            int positiveCounter = 0;
+            int negativeCounter = 0;
+            if (firstNumber > 0) positiveCounter++;
+            else if (firstNumber < 0) negativeCounter++;
+            if (secondNumber > 0) positiveCounter++;
+            else if (secondNumber < 0) negativeCounter++;
+            if (thirdNumber > 0) positiveCounter++;
+            else if (thirdNumber < 0) negativeCounter++;
+
+            Console.WriteLine($"Количество положительных чисел: {positiveCounter}");
+            Console.WriteLine($"Количество отрицательных чисел: {negativeCounter}");
+
         }
     }
 }
