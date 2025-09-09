9fd77e30e5345d67eef0d3c8f20f142fb194d544
BabaYaga123987 <nezenecdmitrij45@gmail.com>
2025-09-09 15:24:59 +0200

    tasks 1, 2, 3 have been changed

--- C#2/Program.cs
+++ C#2/Program.cs
@@ -8,14 +8,18 @@ namespace C_2
         static void Main(string[] args)
         {
             // task 1
-            /*    Console.WriteLine("Введите число целое четырехзначное число:");
-                while (true)
+            Console.WriteLine("Введите целое четырехзначное число:");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out int number))
                 {
-                    string input = Console.ReadLine();
-                    if (int.TryParse(input, out int number) && input.Length == 4)
+                    number = Math.Abs(number); 
+
+                    if (number >= 1000 && number <= 9999) 
                     {
                         int amount = 0;
-                        foreach (char c in input)
+                        foreach (char c in number.ToString())
                         {
                             int digit = c - '0'; // перевод символа в цифру
                             if (digit % 2 == 0)
@@ -31,70 +35,108 @@ namespace C_2
                         Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число из 4 цифр:");
                     }
                 }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число из 4 цифр:");
+                }
+            }
 
-               /* Примечание к упражнению 1. foreach — это цикл, который перебирает все элементы какой-то коллекции 
-                (массива, списка, строки и т. д.), по одному за раз. char c — это переменная, которая на каждой итерации 
-                принимает один символ строки. input — указывает, что мы перебираем строку input. Цикл сам знает, сколько символов, 
-                и закончит работу, когда они закончатся. 
+            /* Примечание к упражнению 1. foreach — это цикл, который перебирает все элементы какой-то коллекции 
+             (массива, списка, строки и т. д.), по одному за раз. char c — это переменная, которая на каждой итерации 
+             принимает один символ строки. input — указывает, что мы перебираем строку input. Цикл сам знает, сколько символов, 
+             и закончит работу, когда они закончатся. 
 
-                */
+             */
 
             // task 2
-            int firstOdd;
-                int secondOdd;
-                int sum;
-                while (true)
+            ulong sum = 0;
+            int a;
+            int b;
+            Console.WriteLine("Введите число а: ");
+            string string1 = Console.ReadLine();
+            while (true)
+            if (!int.TryParse(string1, out a))
+            {
+                Console.WriteLine("Неккоректный ввод: введите целое число а");
+                string1 = Console.ReadLine();
+            }
+            else
+            {
+                break;
+            }            
+            Console.WriteLine("Введите число b: ");
+            string string2 = Console.ReadLine();
+            while (true)
+            {
+                if (!int.TryParse(string2, out b) || a > b)
                 {
-                    Console.WriteLine("Введите первое нечетное целое число:");
-                    if (int.TryParse(Console.ReadLine(), out firstOdd) && firstOdd % 2 != 0)
+                    Console.WriteLine("Неккоректный ввод: введите целое число b. Также b должно быть больше a.");
+                    string2 = Console.ReadLine();
+                }
+                else
+                {
+                    break;
+                }
+            }
+            for (int i = a; i <= b; i++)
+                {
+                    if (i % 2 != 0)
                     {
-                        firstOdd = firstOdd * firstOdd;
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Некорректный ввод. Пожалуйста, введите первое нечетное целое число.");
-                        continue;
+                        sum += (ulong)(i * i);
                     }
                 }
 
+            Console.WriteLine($"Сумма квадратов целых нечетных чисел от a до b равна: {sum}");
 
-                while (true)
+
+
+
+
+
+
+            // task 3
+            Console.WriteLine("Введите пароль: "); //Пароль: 12345
+            const int correctpassword = 12345;
+            int password;
+            int attempts = 5;
+            while (true)
+            {
+                string input1 = Console.ReadLine();
+                if (int.TryParse(input1, out password) && password == correctpassword)
                 {
-                    Console.WriteLine("Введите второе нечетное целое число:");
-                    if (int.TryParse(Console.ReadLine(), out secondOdd) && secondOdd % 2 != 0)
+                    Console.WriteLine("Добро пожаловать!");
+                    return;
+                }
+                else
+                {
+                    if (attempts > 1)
                     {
-                        secondOdd = secondOdd * secondOdd;
-                        break;
+                        attempts--;
+                        Console.WriteLine($"Ошибка: пароль неверный. Осталось попыток: {attempts}");
                     }
                     else
                     {
-                        Console.WriteLine("Некорректный ввод. Пожалуйста, введите второе нечетное целое число.");
-                        continue;
+                        Console.WriteLine("Достигнут лимит попыток");
+                        return;
                     }
                 }
+            }
 
-                sum = firstOdd + secondOdd;
-                Console.WriteLine($"Сумма квадратов двух нечетных чисел: {sum}");
-
-
+            
 
 
+            
 
 
-                // task 3
-                int attempts = 0;
-                Console.WriteLine("Введите пароль:"); // Пароль: 12345
-                while (true)
-                {
-                    if (Console.ReadLine() == "12345")
-                    {
-                        Console.WriteLine("Добро пожаловать!");
-                        break;
-                    }
-                    else
-                    {
-                        attempts++;
+
+
+
+
+
+
+
+
+                       /* attempts++;
                         if (attempts != 5)
                         {
                             Console.WriteLine("Пароль неверный. Изначально вы можете повторить ввод до 5 раз.");
@@ -103,11 +145,11 @@ namespace C_2
                         else
                         {
                             break;
-                        }
+                        } */
                     }
                 }
             }
-        }
-    }
+        
+    
 
 
