af1e710bf908545fb056bde6403400bdbe46ece9
BabaYaga123987 <nezenecdmitrij45@gmail.com>
2025-09-10 12:00:49 +0200

    tasks 1, 2, 3, 4 have been rebuilt.

--- C#2/Program.cs
+++ C#2/Program.cs
@@ -6,10 +6,11 @@ namespace C_2
     {
         static void Main(string[] args)
         {
-
-            int square;
+            // task 1
+            
             for (int number = 10; number < 21; number++)
             {
+                int square;
                 square = number * number;
                 Console.WriteLine(square);
             }
@@ -17,89 +18,68 @@ namespace C_2
 
 
 
-
-            int n = 0;
-            long sum = 0;
-
-            Console.WriteLine("Введите число n:");
-
+            // task 2
+            Console.WriteLine("Введите число n (n > 1):");
             while (true)
             {
-                try
+                int n;
+                string input = Console.ReadLine();
+                if (!int.TryParse(input, out n) || n <= 1)
                 {
-                    n = int.Parse(Console.ReadLine());
-                    break;
-                }
-                catch (OverflowException ex)
-                {
-                    Console.WriteLine("Ошибка: введенное число слишком большое или слишком маленькое.");
+                    Console.WriteLine("Ошибка ввода. Пожалуйста, введите целое число n (n > 1).");
                     continue;
                 }
-                catch (Exception ex)
+                if (n > 1)
                 {
-                    Console.WriteLine("Ошибка ввода. Пожалуйста, введите целое число.");
-                    continue;
-                }
-            }
-            if (n > 0)
-            {
-                for (int i = 1; i <= n; i++)
-                {
-                    sum += i;
-                }
-                Console.WriteLine($"Сумма чисел от 1 до {n} равна: {sum}");
-            }
-            else if (n < 0)
-            {
-                for (int i = 1; i >= n; i--)
-                {
-                    sum += i;
-                }
-                Console.WriteLine($"Сумма чисел от 1 до {n} равна: {sum}");
-            }
-            else
-            {
-                Console.WriteLine("Сумма чисел от 1 до 0 равна: 0");
-            }
-
-
-
-
-            for (int number2 = 20; ; number2++)
-            {
-                if (number2 == 50)
-                {
+                    ulong sum = 0;
+                    for (uint i = 1; i <= n; i++)
+                    {
+                        sum += i;
+                    }
+                    Console.WriteLine($"Сумма чисел от 1 до {n} равна: {sum}");
                     break;
                 }
-                else if (number2 % 3 == 0 && number2 % 5 != 0)
+            }
+
+
+            // task 3
+            for (int number2 = 20; number2 < 50; number2++)
+            {
+                if (number2 % 3 == 0 && number2 % 5 != 0)
                 {
                     Console.WriteLine(number2);
                 }
             }
 
 
-
+            // task 4
             Console.WriteLine("Введите целое число:");
 
-
+            
             while (true)
-            { 
-                if (!long.TryParse(Console.ReadLine(), out long number3))
             {
-                Console.WriteLine("Ошибка: введите корректное целое число");
-                    continue;
-            }
-                long dividednumber;
-                do
+                if (!long.TryParse(Console.ReadLine(), out long number3))
                 {
-                    dividednumber = Math.Abs(number3 % 10);
-                    Console.WriteLine(dividednumber);
-                    number3 = number3 / 10;
+                    Console.WriteLine("Ошибка: введите корректное целое число");
+                    continue;
+                }
+                else
+                {
+                    do
+                    {
+                        long dividednumber;
+                        dividednumber = Math.Abs(number3 % 10);
+                        Console.WriteLine(dividednumber);
+                        number3 = number3 / 10;
 
-                } while (number3 != 0);
+                    } while (number3 != 0);
+                    break;
+                }
 
-        }
+            }
 
+
+            
         }
     }
 }
